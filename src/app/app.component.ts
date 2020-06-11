f8adedc3e62c5b602e8ec4b5e532e9ed757dbdb0
Aseem Kohli <aseemko@google.com>
2020-06-11 09:20:32 -0400

    Add post class and display in root template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { Post } from "./post";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  title = 'blog-app';
+  title = "blog-app";
+  posts: Post[] = [
+    new Post("My first angular app", "body of my first angular app."),
+    new Post(
+      "My first angular component",
+      "body of my first angular component."
+    ),
+  ];
 }
---
+++ src/app/post.ts
@@ -0,0 +1,9 @@
+export class Post {
+  title: string;
+  body: string;
+
+  constructor(title: string, body: string) {
+    this.title = title;
+    this.body = body;
+  }
+}
