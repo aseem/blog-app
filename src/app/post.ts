aca572d1abd05bbc8f7c0cf827a985ae665d4de1
Aseem Kohli <aseemko@google.com>
2020-06-12 08:15:22 -0400

    Add created property to post class

--- src/app/post.ts
+++ src/app/post.ts
@@ -2,10 +2,12 @@ export class Post {
   title: string;
   body: string;
   likes: number;
+  created: Date;
 
   constructor(title: string, body: string) {
     this.title = title;
     this.body = body;
     this.likes = 0;
+    this.created = new Date("2020-06-11");
   }
 }
