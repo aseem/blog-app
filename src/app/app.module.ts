575f55e4b5f743cb3c756c3261e543e2fe18f09d
Aseem Kohli <aseemko@google.com>
2020-06-12 09:09:45 -0400

    add basic compose form without any styling or material components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
-
+import { FormsModule } from "@angular/forms";
 import { AppComponent } from "./app.component";
 import { PostListComponent } from "./post-list/post-list.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
@@ -10,9 +10,9 @@ import { MatIconModule } from "@angular/material/icon";
 import { MatBadgeModule } from "@angular/material/badge";
 import { PostCardComponent } from "./post-card/post-card.component";
 import { LikeButtonComponent } from "./like-button/like-button.component";
-import { FuzzyDatePipe } from './fuzzy-date.pipe';
-import { AppRoutingModule } from './app-routing.module';
-import { ComposeComponent } from './compose/compose.component';
+import { FuzzyDatePipe } from "./fuzzy-date.pipe";
+import { AppRoutingModule } from "./app-routing.module";
+import { ComposeComponent } from "./compose/compose.component";
 
 @NgModule({
   declarations: [
@@ -31,6 +31,7 @@ import { ComposeComponent } from './compose/compose.component';
     MatIconModule,
     MatBadgeModule,
     AppRoutingModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/compose/compose.component.ts
+++ src/app/compose/compose.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from "@angular/core";
+import { NgForm } from "@angular/forms";
+import { Post } from "../post";
 
 @Component({
-  selector: 'app-compose',
-  templateUrl: './compose.component.html',
-  styleUrls: ['./compose.component.css']
+  selector: "app-compose",
+  templateUrl: "./compose.component.html",
+  styleUrls: ["./compose.component.css"],
 })
 export class ComposeComponent implements OnInit {
+  @ViewChild("composeForm") composeForm: NgForm;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  handleSubmission() {
+    console.log(this.composeForm.value);
+    let newPost = new Post(
+      this.composeForm.value["title"],
+      this.composeForm.value["body"]
+    );
+
+    this.composeForm.reset();
   }
-
 }
